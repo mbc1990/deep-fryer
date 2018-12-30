b0e9c5e6adf97d81c0fdf4af150ed78c4796b67b
mbc <malcolmbalch@gmail.com>
2018-12-30 13:19:00 -0500

    basic, working decode/encode loop

--- deep_fryer/src/main.rs
+++ deep_fryer/src/main.rs
@@ -1,33 +1,45 @@
 extern crate image;
-use image::GenericImageView;
+extern crate rand;
 use image::jpeg::{JPEGDecoder, JPEGEncoder};
 use std::fs::File;
 use image::ImageDecoder;
 use image::DecodingResult;
 use image::ColorType;
+use rand::Rng;
 
-// Compiles and runs, but produces nonsense output
+
 fn main() {
-    let f = File::open("test.jpg").unwrap();
-    let mut decoder = JPEGDecoder::new(f);
-    let dims = decoder.dimensions().unwrap();
-    println!("ct: {:?}", decoder.colortype());
-    let img = decoder.read_image().unwrap();
 
-    match img {
-        DecodingResult::U8(to_encode) => {
-            println!("Processing vec of u8s");
-            let mut f_out = File::create("test_encoded.jpg").unwrap();
-            let mut encoder = JPEGEncoder::new_with_quality(&mut f_out, 100);
-            encoder.encode(
-                to_encode.as_slice(),
-                decoder.dimensions().unwrap().0,
-                decoder.dimensions().unwrap().1,
-                ColorType::RGB(8)
-            );
-        },
-        DecodingResult::U16(vec_of_u16s) => {
-            println!("Got a u16, sorry");
-        },
+    // TODO: Do this with reusable buffers, not reading/writing to filesystem each time
+    let mut rng = rand::thread_rng();
+    for i in 0..500 {
+        let f = File::open("test.jpg").unwrap();
+        let mut decoder = JPEGDecoder::new(f);
+        let dims = decoder.dimensions().unwrap();
+        let img = decoder.read_image().unwrap();
+
+        match img {
+            DecodingResult::U8(to_encode) => {
+                let mut f_out = File::create("test.jpg").unwrap();
+                let quality = rng.gen_range(70, 80);
+                println!("Iteration: {} with quality: {}", i, quality);
+                let mut encoder = JPEGEncoder::new_with_quality(&mut f_out, quality);
+                let res = encoder.encode(
+                    to_encode.as_slice(),
+                    dims.0,
+                    dims.1,
+                    ColorType::RGB(8)
+                );
+                match res {
+                    Ok(_) => {},
+                    Err(err) => {
+                        println!("Err: {}", err);
+                    }
+                }
+            },
+            DecodingResult::U16(_to_encode) => {
+                println!("Got a u16, sorry");
+            },
+        }
     }
 }
